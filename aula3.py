8a369ed37f049dfe40d646cdb6e50357008086e5
SamuelMends <samuelqmendes@gmail.com>
2024-05-11 17:45:35 -0300

    comit

---
+++ aula3.py
@@ -0,0 +1,18 @@
+"""
+Python = Linguagem de programação 
+Tipo de tipagem = Dinâmica/Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas.
+"""
+
+#Aspas simples
+print('Luiz Otavio')
+
+#Aspas duplas
+print("Luiz Otavio")
+
+#Escape (Adiciona aspas a frase)
+print("Luiz \"Otavio\"")
+
+#r Adiciona as barras
+print(r"Luiz \"Otavio\"")
