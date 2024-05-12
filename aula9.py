ec012ee919d5b04768e72158c3dc9933e0361c68
SamuelMends <samuelqmendes@gmail.com>
2024-05-11 22:09:45 -0300

    aula9.py

---
+++ aula9.py
@@ -0,0 +1,21 @@
+adicao = 5+5 
+print('Adição {}'.format(adicao))
+
+subtracao = 10-5
+print('Subtração {}'.format(subtracao))
+
+multiplicacao = 10*10
+print('Multiplicação {}'.format(multiplicacao))
+
+divisao = 10/3
+print('Divisão {:.2f}'.format(divisao))
+
+divisao_inteira = 10 // 3
+print('Divisão inteira {}'.format(divisao_inteira))
+
+exponenciacao = 2 ** 10
+print('Exponenciação {}'.format(exponenciacao))
+
+modulo = 55 % 2 #Resto da divisão
+print('Módulo {}'.format(modulo))
+
